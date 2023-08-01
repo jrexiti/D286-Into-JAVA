f3f1e730d94901bb357456a39bd4d4363296017e
jrexiti <jay.rexiti@gmail.com>
2023-07-31 21:48:26 -0400

    q01

---
+++ D286-Intro-JAVA/src/main/java/com/example/Assignment01.java
@@ -0,0 +1,21 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Assignment01 {
+    public static void main(String[] args) {
+        String[] lines = {
+                "H   H",
+                "H   H",
+                "HHHHH",
+                "H   H",
+                "H   H"
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
