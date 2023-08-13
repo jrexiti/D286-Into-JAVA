cd49d6c7e117a94aaa1c33e36bb073598a90e40e
jrexiti <jay.rexiti@gmail.com>
2023-08-13 02:11:53 -0400

    assessment 06

---
+++ src/main/java/com/example/assessment06.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class assessment06 {
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+
+        String fullName = scnr.nextLine();
+        String splitName[] = fullName.split(" ");
+
+        String output = splitName[splitName.length - 1].charAt(0) + ", " + splitName[0];
+        if (splitName.length > 2) {
+
+            output += " " + splitName[1].charAt(0) + ".";
+
+        }
+        System.out.println(output);
+        scnr.close();
+    }
+}
