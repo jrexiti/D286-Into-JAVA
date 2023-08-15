6f667024bbecbf795bf9eedf0234435587da2c4b
jrexiti <jay.rexiti@gmail.com>
2023-08-15 14:23:27 -0400

    q4

---
+++ src/main/java/com/example/PersonsDetail_04.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class PersonsDetail_04 {
+
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+
+        String fullName = scnr.nextLine();
+        int age = scnr.nextInt();
+        String salary = scnr.next();
+
+        System.out.println(fullName + " is " + age + " and makes $" + salary + ".");
+
+    }
+}
